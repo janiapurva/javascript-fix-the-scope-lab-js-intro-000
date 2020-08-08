997fcfdc6a99aa9e130abe2714311fa7bb51187f
APURVA MAHNEDRABHAI JANI <jani.apurva@yahoo.com>
2020-08-08 22:32:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-/ 
+/
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope
   // How can we make sure that this function
   // and the above function both pass?
